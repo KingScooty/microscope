68794e4a778a5f5a40bcd20b5393d9ce2832ac84
Scotty Vernon <scott.vernon@gmail.com>
2014-05-15 22:36:15 +0100

    Denomralised the number of comments into the post.

--- collections/comments.js
+++ collections/comments.js
@@ -4,6 +4,7 @@ Meteor.methods({
   comment: function(commentAttributes) {
     var user = Meteor.user();
     var post = Posts.findOne(commentAttributes.postId);
+    
     // ensure the user is logged in
     if (!user)
       throw new Meteor.Error(401, "You need to login to make comments");
@@ -11,11 +12,15 @@ Meteor.methods({
       throw new Meteor.Error(422, "Please write some content");
     if (!post)
       throw new Meteor.Error(422, "You must comment on a post");
+    
     comment = _.extend(_.pick(commentAttributes, 'postId', 'body'), {
       userId: user._id,
       author: user.username,
-      submitted: new Date().getTime()
+      submitted: new Date().getTime(),
     });
+
+    Posts.update(comment.postId, {$inc: {commentsCount: 1}});
+
     return Comments.insert(comment);
   }
 });
--- collections/posts.js
+++ collections/posts.js
@@ -33,22 +33,12 @@ Meteor.methods({
     }
 
     // pick out the whitelisted keys
-    var post = _.extend(_.pick(postAttributes, 'url', 'message'), {
-      title: postAttributes.title + (this.isSimulation ? '(client)' : '(server)'),
+    var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), {
       userId: user._id, 
       author: user.username, 
-      submitted: new Date().getTime()
+      submitted: new Date().getTime(),
+      commentsCount: 0
     });
-
-    // wait for 5 seconds
-    if (! this.isSimulation) {
-      var Future = Npm.require('fibers/future');
-      var future = new Future();
-      Meteor.setTimeout(function() {
-        future.return();
-      }, 5 * 1000);
-      future.wait();
-    }
 
     var postId = Posts.insert(post);
     return postId;
